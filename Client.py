5065f80919392439a478c4a132931022106fe0d0
ikmal <ikmalazman65@gmail.com>
2022-01-24 05:29:05 +0000

    Client

--- Client.py
+++ Client.py
@@ -3,6 +3,92 @@ import sys
 HOST = "192.168.56.103"
 PORT = 8888
 
+
+
+def rocks(x,y):
+      global win,loss
+      move = x
+      opponent = y
+
+      if move == opponent:
+
+              print("It's Tie\n")
+              win = win + 1
+              loss = loss + 1
+              print("Player:",win,"\tOpponent:",loss,"\n")
+
+      elif move == 'r' and opponent == 's':
+
+              print("You win\n")
+              win = win + 1
+              print("Player:" ,win, "\tOpponent:", loss,"\n")
+
+      elif move == 'r' and opponent == 'p':
+
+              print("You lose\n")
+              loss = loss + 1
+              print("Player:",win,"\tOpponent",loss,"\n")
+
+      else:
+              print("Invalid move Rocks\n")
+
+
+def paper(x,y):
+      global win,loss
+      move = x
+      opponent = y
+
+      if move == opponent:
+               print("It's a Tie\n")
+
+               win = win + 1
+               loss = loss + 1
+               print("Player:",win,"\tOpponent:",loss,"\n")
+
+      elif move == 'p' and opponent == 's':
+
+               print("You lose\n")
+               loss = loss + 1
+               print("Player:" , win,"\tOpponent:", loss,"\n")
+
+      elif move == 'p' and opponent == 'r':
+
+               print("You win\n")
+               win = win + 1
+               print("Player:",win,"\tOpponent:",loss,"\n")
+      else:
+           print("Invalid move Paper\n")
+
+
+
+def scissor(x,y):
+      global win,loss
+      move = x
+      opponent = y
+
+      if move == opponent:
+               print("It's a Tie\n")
+
+               win = win + 1
+               loss = loss + 1
+               print("Player:",win,"\tOpponent:",loss,"\n")
+
+      elif move == 's' and opponent == 'p':
+
+               print("You win\n")
+               win = win + 1
+               print("Player:" , win,"\tOpponent:", loss,"\n")
+
+      elif move == 's' and opponent == 'r':
+               print("You lose\n")
+               loss = loss + 1
+
+               print("Player:",win,"\tOpponent:",loss,"\n")
+      else:
+               print("Invalid move Scissors")
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         
